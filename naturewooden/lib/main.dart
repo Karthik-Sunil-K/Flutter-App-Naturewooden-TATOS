be016f4cc1d9b54cf1ae7bec8979c94706881926
Karthik Sunil K <karthiksunil038@gmail.com>
2021-05-04 00:41:31 +0530

    email input border color not changes

--- naturewooden/lib/main.dart
+++ naturewooden/lib/main.dart
@@ -27,26 +27,63 @@ class MyApp extends StatelessWidget {
                     padding: EdgeInsets.fromLTRB(20, 30, 0, 0),
                     child: Text(
                       'Welcome Back',
-                      style: TextStyle(color: Color(0xff633820),fontSize: 30,fontWeight: FontWeight.w600),
+                      style: TextStyle(
+                          color: Color(0xff633820),
+                          fontSize: 30,
+                          fontWeight: FontWeight.w600),
                     ),
                   ),
                   Container(
                     padding: EdgeInsets.fromLTRB(20, 79, 0, 0),
                     child: Text(
                       'Signing up or login to see \nour top pics for you',
-                      style: TextStyle(color: Color(0xff633820),fontSize: 18),
+                      style: TextStyle(color: Color(0xff633820), fontSize: 18),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(20, 160, 0, 0),
+                    child: Text(
+                      'Email Address',
+                      style: TextStyle(color: Color(0xff633820), fontSize: 18),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(20, 210, 20, 0),
+                    child: Column(
+                      children: [
+                        TextField(
+                          decoration: InputDecoration(
+                    
+                            
+                            focusedBorder: InputBorder.none,
+                            focusColor: Color(0xff633820),
+                            // enabledBorder: InputBorder.none,
+                            hintText:'GuySimmmons@gmail.com',
+                            // labelText: "Enter Email",
+                            labelStyle: TextStyle(color: Color(0xff633820)),
+                            fillColor: Colors.grey,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5.0),
+                              borderSide: BorderSide(
+                                color: Colors.green,
+                                width: 50,
+                              ),
+        
+                            ),
+                            //fillColor: Colors.green
+                          ),
+                        
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                      ],
                     ),
                   ),
                 ],
               ),
-            )
+            ),
           ],
         ),
       ),
     );
   }
 }
-
-
-
-
